691d0d00d9657dd851c42336eb3330b9ec41a315
Han-nahh <hannatesfaye11@gmail.com>
2023-04-15 00:39:33 +0300

    webhook

--- api/echo.py
+++ api/echo.py
@@ -1,10 +1,30 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram import Update, Bot
 import os
+from typing import Optional
 
 ##TOKEN = os.environ.get("5814904336:AAG1dikS2jVjgcpyQ2xC_Y-d1OBHGj4UAdU")
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TelegramWebhook(BaseModel):
+    '''
+    Telegram Webhook Model using Pydantic for request body validation
+    '''
+    update_id: int
+    message: Optional[dict]
+    edited_message: Optional[dict]
+    channel_post: Optional[dict]
+    edited_channel_post: Optional[dict]
+    inline_query: Optional[dict]
+    chosen_inline_result: Optional[dict]
+    callback_query: Optional[dict]
+    shipping_query: Optional[dict]
+    pre_checkout_query: Optional[dict]
+    poll: Optional[dict]
+    poll_answer: Optional[dict]
 
 @app.get("/")
 def index():
@@ -17,6 +37,26 @@ def start(update, context):
 def register_handlers(dispatcher):
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
+
+@app.post("/webhook")
+def webhook(webhook_data: TelegramWebhook):
+    '''
+    Telegram Webhook
+    '''
+    # Method 1
+    bot = Bot(token=TOKEN)
+    update = Update.de_json(webhook_data, app.bot)
+    dispatcher = Dispatcher(bot, None, workers=4)
+    register_handlers(dispatcher)
+
+    # handle webhook request
+    dispatcher.process_update(update)
+
+    return {"message": "ok"}
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
 
 def main():
     updater = Updater(token='5814904336:AAG1dikS2jVjgcpyQ2xC_Y-d1OBHGj4UAdU', use_context=True)
